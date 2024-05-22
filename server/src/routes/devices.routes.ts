d78a979231deb1530b847614bb697e964c1bb9ab
biahellens <60991740+Biahellens@users.noreply.github.com>
2024-05-22 10:30:04 -0300

    :sparkles: feat(): Adicionando routes

---
+++ server/src/routes/devices.routes.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import { DeviceController } from '../controllers/device.controller';
+import { authentification } from '../middleware/authentification';
+
+const Router = express.Router();
+
+// Rotas para dispositivos
+Router.get('/allDevices', authentification, DeviceController.getAllDevices);
+Router.post('/createDevice', authentification, DeviceController.createDevice);
+Router.get('/getDevicesByUser', authentification, DeviceController.getDevicesByUser);
+Router.delete('/:id/deleted', authentification, DeviceController.deleteDevice);
+Router.put("/addCommands", authentification, DeviceController.addCommand);
+
+
+export { Router as deviceRouter }
