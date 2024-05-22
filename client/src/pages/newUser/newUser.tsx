313fbe38049b2c26f1a5af1a23aca3bd73bd8b97
biahellens <60991740+Biahellens@users.noreply.github.com>
2024-05-22 11:25:00 -0300

    :sparkles: feat(): Adicionando integração create new user

--- client/src/pages/newUser/newUser.tsx
+++ client/src/pages/newUser/newUser.tsx
@@ -1,27 +1,23 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { UseMobile } from "@contexts/MobileContext";
-import {
-  Button,
-  Stack,
-  Typography,
-  OutlinedInput
-} from "@mui/material";
+import { User } from "@models/User";
+import { Button, Stack, Typography, OutlinedInput } from "@mui/material";
+import { UserService } from "@services/userService";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-
-// Icons
-
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function NewUser() {
   const { isMobile } = UseMobile();
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const [formData, setFormData] = useState({
     id: 0,
-    name: '',
-    email: '',
-    password: ''
-  })
+    name: "",
+    email: "",
+    password: "",
+  });
 
   const handleChange = (event: any) => {
     const { name, value } = event.target;
@@ -29,6 +25,41 @@ export default function NewUser() {
       ...prevState,
       [name]: value,
     }));
+  };
+
+  const createUser = async () => {
+    if (!formData?.email || !formData?.name || !formData?.password) {
+      toast.info("Por favor, preencha todos os campos.");
+      return;
+    }
+
+    const userBody: User = {
+      id: 0,
+      name: formData.name,
+      email: formData.email.toLocaleLowerCase(),
+      password: formData.password,
+    };
+
+    try {
+      await UserService.RegisterUser(userBody);
+
+      toast.success("Usuário criado com sucesso!", {
+        style: {
+          backgroundColor: "#55B938",
+          color: "white",
+        },
+        theme: "colored",
+      });
+
+      setFormData({
+        id: 0,
+        name: "",
+        email: "",
+        password: "",
+      });
+    } catch (error) {
+      toast.warn("Ocorreu um erro ao criar usuário.");
+    }
   };
 
   return (
@@ -73,68 +104,97 @@ export default function NewUser() {
           marginBottom: isMobile ? "2rem" : 0,
         }}
       >
-        <Stack width='100%' flexDirection='row' justifyContent='space-between'>
-          <Stack width='48%'>
-            <Typography sx={{
-              fontSize: '1rem',
-              marginBottom: '0.5rem'
-            }}>Nome:</Typography>
-            <OutlinedInput placeholder="Digite seu nome..." name='name' value={formData.name} onChange={handleChange} />
+        <Stack width="100%" flexDirection="row" justifyContent="space-between">
+          <Stack width="48%">
+            <Typography
+              sx={{
+                fontSize: "1rem",
+                marginBottom: "0.5rem",
+              }}
+            >
+              Nome:
+            </Typography>
+            <OutlinedInput
+              placeholder="Digite seu nome..."
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+            />
           </Stack>
-          <Stack width='48%'>
-            <Typography sx={{
-              fontSize: '1rem',
-              marginBottom: '0.5rem'
-            }}>Senha:</Typography>
-            <OutlinedInput placeholder="Digite sua senha..." name='password' value={formData.password} onChange={handleChange}/>
+          <Stack width="48%">
+            <Typography
+              sx={{
+                fontSize: "1rem",
+                marginBottom: "0.5rem",
+              }}
+            >
+              Senha:
+            </Typography>
+            <OutlinedInput
+              placeholder="Digite sua senha..."
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+            />
           </Stack>
         </Stack>
-        <Stack width='100%' mt='2rem'>
-        <Typography sx={{
-              fontSize: '1rem',
-              marginBottom: '0.5rem'
-            }}>Email</Typography>
+        <Stack width="100%" mt="2rem">
+          <Typography
+            sx={{
+              fontSize: "1rem",
+              marginBottom: "0.5rem",
+            }}
+          >
+            Email
+          </Typography>
 
-          <OutlinedInput placeholder="Digite seu email..." name='email' value={formData.email} onChange={handleChange} />
+          <OutlinedInput
+            placeholder="Digite seu email..."
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+          />
         </Stack>
       </Stack>
       <Stack
-          flexDirection={isMobile ? "column" : "row"}
-          justifyContent={isMobile ? "center" : "space-between"}
-          alignItems={"center"}
-          mt={"5rem"}
+        flexDirection={isMobile ? "column" : "row"}
+        justifyContent={isMobile ? "center" : "space-between"}
+        alignItems={"center"}
+        mt={"5rem"}
+      >
+        <Button
+          onClick={() => navigate("/users")}
+          sx={{
+            width: isMobile ? "80%" : "13rem",
+            background: "#8B0000",
+            color: "#FFFFFF",
+            "&:hover": {
+              background: "#FFFFFF",
+              color: "#8B0000",
+              border: "1px solid #8B0000",
+            },
+          }}
         >
-          <Button
-            onClick={() => navigate('/users')}
-            sx={{
-              width: isMobile ? "80%" : "13rem",
-              background: "#8B0000",
-              color: "#FFFFFF",
-              "&:hover": {
-                background: "#FFFFFF",
-                color: "#8B0000",
-                border: "1px solid #8B0000",
-              },
-            }}
-          >
-            Cancelar
-          </Button>
-          <Button
-            sx={{
-              marginTop: isMobile ? "1rem" : 0,
-              width: isMobile ? "80%" : "13rem",
-              background: "#6B8E23",
-              color: "#FFFFFF",
-              "&:hover": {
-                background: "#FFFFFF",
-                color: "#6B8E23",
-                border: "1px solid #6B8E23",
-              },
-            }}
-          >
-            Salvar
-          </Button>
+          Cancelar
+        </Button>
+        <Button
+          onClick={() => createUser()}
+          sx={{
+            marginTop: isMobile ? "1rem" : 0,
+            width: isMobile ? "80%" : "13rem",
+            background: "#6B8E23",
+            color: "#FFFFFF",
+            "&:hover": {
+              background: "#FFFFFF",
+              color: "#6B8E23",
+              border: "1px solid #6B8E23",
+            },
+          }}
+        >
+          Salvar
+        </Button>
       </Stack>
+      <ToastContainer />
     </Stack>
   );
 }
